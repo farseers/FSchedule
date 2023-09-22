3348a1a3c92bd035e12ff8fc88721168988a5d74
steden <1470804@qq.com>
2023-09-22 22:54:11 +0800

    修改脚本

--- pushLocalHub.sh
+++ pushLocalHub.sh
@@ -1,8 +1,4 @@
 # 本地打包并上传到内网hub，用于测试部署到容器的效果
-docker kill fschedule
-docker rm fschedule
-docker rmi fschedule:latest
-
 # 编译应用
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o fschedule-server -ldflags="-w -s" .
 # 打包

9b6414f88b4f995280e9ca110d78fb44ca9e6d0f
steden <1470804@qq.com>
2023-09-28 12:36:19 +0800

    升级框架

--- infrastructure/repository/context/mysqlContext.go
+++ infrastructure/repository/context/mysqlContext.go
@@ -9,12 +9,12 @@ import (
 var MysqlContextIns *mysqlContext
 
 type mysqlContext struct {
-	TaskGroup data.TableSet[model.TaskGroupPO] `data:"name=fschedule_task_group"`
-	Task      data.TableSet[model.TaskPO]      `data:"name=fschedule_task"`
-	TaskLog   data.TableSet[model.TaskLogPO]   `data:"name=fschedule_task_log"`
+	TaskGroup data.TableSet[model.TaskGroupPO] `data:"name=fschedule_task_group;migrate;"`
+	Task      data.TableSet[model.TaskPO]      `data:"name=fschedule_task;migrate;"`
+	TaskLog   data.TableSet[model.TaskLogPO]   `data:"name=fschedule_task_log;migrate;"`
 }
 
 // InitMysqlContext 初始化上下文
 func InitMysqlContext() {
-	MysqlContextIns = data.NewContext[mysqlContext]("default", true)
+	MysqlContextIns = data.NewContext[mysqlContext]("default")
 }

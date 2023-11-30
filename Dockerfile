c39bab80b77dcafebc7187ba503e0f3d004296f2
steden <1470804@qq.com>
2023-11-30 12:55:59 +0800

    修改本地部署脚本和Dockerfile文件

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,38 @@
+# 注意，这里的构建上下文，是在git源代码的根目录
+FROM golang:1.20.11-alpine AS build
+# 设置github代理
+ENV GOPROXY https://goproxy.cn,direct
+# 进入到项目目录中
+WORKDIR /src
+# 复制go.mod文件
+COPY ./go.mod .
+# 下载依赖（支持docker缓存）
+RUN go mod download
+
+# 设置src目录，并将源代码复制到此
+COPY . .
+
+# 删除go.work文件
+RUN rm -rf go.work
+
+# 更新go.sum
+RUN go mod tidy
+
+# 编译
+RUN GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o /app/fschedule-server -ldflags="-w -s" .
+
 FROM alpine:latest AS base
 WORKDIR /app
-COPY ./fschedule-server .
-# 复制配置
-COPY ./farseer.yaml .
-# 复制视图
-#COPY ./views ./views
-# 复制静态资源
-#COPY ./wwwroot ./wwwroot
+COPY --from=build /app .
+# 复制配置（没有配置需要注释掉）
+COPY --from=build /src/farseer.yaml .
+# 复制视图（没有视图需要注释掉）
+#COPY --from=build /src/views ./views
+# 复制静态资源（没有静态资源需要注释掉）
+#COPY --from=build /src/wwwroot ./wwwroot
 
 #设置时区
-ENV TZ=Asia/Shanghai
-RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai    /etc/localtime
 
 ENTRYPOINT ["./fschedule-server"]
+
--- pushLocalHub.sh
+++ pushLocalHub.sh
@@ -1,10 +1,26 @@
-# 本地打包并上传到内网hub，用于测试部署到容器的效果
-# 编译应用
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o fschedule-server -ldflags="-w -s" .
-# 打包
-docker build -t fschedule:latest --network=host .
+# 更新farseer-go框架
+cd ../farseer-go && sh git-update.sh
+# 更新FSchedule
+cd ../FSchedule && git pull
+# 生成路由
+fsctl -r
+# 将忽略文件复制到上下文根目录中
+#\cp .dockerignore ../
+# 编译
+docker build -t fschedule:latest --network=host -f ./Dockerfile ../
 # 发到内网
-docker tag fschedule:latest hub.fsgit.cc/fschedule:dev
-docker push hub.fsgit.cc/fschedule:dev
-docker rmi hub.fsgit.cc/fschedule:dev
-docker rmi fschedule:latest
+docker tag fschedule:latest hub.fsgit.cc/fschedule:dev && docker push hub.fsgit.cc/fschedule:dev
+docker rmi hub.fsgit.cc/fschedule:dev && docker rmi fschedule:latest
+
+# docker
+docker service rm fschedule
+docker service create --name fschedule --replicas 1 -d --network=net \
+--constraint node.role==worker \
+--mount type=bind,src=/etc/localtime,dst=/etc/localtime \
+-e "Database_default=\"DataType=mysql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:123456@tcp(127.0.0.1:3306)/fschedule?charset=utf8&parseTime=True&loc=Local\"" \
+-e "Redis_default=\"Server=127.0.0.1:6379,DB=14,Password=123456,ConnectTimeout=600000,SyncTimeout=10000,ResponseTimeout=10000\"" \
+-l "traefik.http.routers.fschedule.rule=Host(\`fschedule.fsgit.cc\`)" \
+-l "traefik.http.routers.fschedule.entrypoints=websecure" \
+-l "traefik.http.routers.fschedule.tls=true" \
+-l "traefik.http.services.fschedule.loadbalancer.server.port=8886" \
+hub.fsgit.cc/fschedule:dev

b716f89eafddb8ceb0ac5bccd9123b37218c7d07
steden <1470804@qq.com>
2023-11-30 13:56:18 +0800

    修改本地部署脚本和Dockerfile文件

--- pushLocalHub.sh
+++ pushLocalHub.sh
@@ -17,8 +17,8 @@ docker service rm fschedule
 docker service create --name fschedule --replicas 1 -d --network=net \
 --constraint node.role==worker \
 --mount type=bind,src=/etc/localtime,dst=/etc/localtime \
--e "Database_default=\"DataType=mysql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:123456@tcp(127.0.0.1:3306)/fschedule?charset=utf8&parseTime=True&loc=Local\"" \
--e "Redis_default=\"Server=127.0.0.1:6379,DB=14,Password=123456,ConnectTimeout=600000,SyncTimeout=10000,ResponseTimeout=10000\"" \
+-e "Database_default=\"DataType=mysql,PoolMaxSize=25,PoolMinSize=1,ConnectionString=root:steden@123@tcp(192.168.1.8:3306)/fschedule?charset=utf8&parseTime=True&loc=Local\"" \
+-e "Redis_default=\"Server=192.168.1.8:6379,DB=14,Password=steden@123,ConnectTimeout=600000,SyncTimeout=10000,ResponseTimeout=10000\"" \
 -l "traefik.http.routers.fschedule.rule=Host(\`fschedule.fsgit.cc\`)" \
 -l "traefik.http.routers.fschedule.entrypoints=websecure" \
 -l "traefik.http.routers.fschedule.tls=true" \

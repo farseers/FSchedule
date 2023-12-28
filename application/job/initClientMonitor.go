c8ec865133d9a92dbe07a95d8871c0701bc4bb08
steden <1470804@qq.com>
2023-12-28 23:20:42 +0800

    新增：JOB的链路追踪

--- application/job/initClientMonitor.go
+++ application/job/initClientMonitor.go
@@ -4,10 +4,15 @@ import (
 	"FSchedule/domain"
 	"FSchedule/domain/client"
 	"github.com/farseer-go/fs/container"
+	"github.com/farseer-go/fs/trace"
 )
 
 // InitClientMonitor 初始化客户端
 func InitClientMonitor() {
+	// 链路追踪
+	traceContext := container.Resolve[trace.IManager]().EntryTask("初始化客户端")
+	defer traceContext.End()
+
 	clientRepository := container.Resolve[client.Repository]()
 	// 检查所有客户端
 	clientRepository.ToList().Foreach(func(clientDO *client.DomainObject) {
--- application/job/initTaskGroupMonitor.go
+++ application/job/initTaskGroupMonitor.go
@@ -4,10 +4,15 @@ import (
 	"FSchedule/domain"
 	"FSchedule/domain/taskGroup"
 	"github.com/farseer-go/fs/container"
+	"github.com/farseer-go/fs/trace"
 )
 
 // InitTaskGroupMonitor 初始化任务组监听
 func InitTaskGroupMonitor() {
+	// 链路追踪
+	traceContext := container.Resolve[trace.IManager]().EntryTask("初始化任务组监听")
+	defer traceContext.End()
+
 	repository := container.Resolve[taskGroup.Repository]()
 	repository.ToList().Foreach(func(taskGroupDO *taskGroup.DomainObject) {
 		domain.MonitorTaskGroupPush(taskGroupDO)

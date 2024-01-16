b398c5de69295d7db5042c13694d2dabeb86a75a
steden <1470804@qq.com>
2024-01-16 20:50:59 +0800

    忽略部份事件

--- application/domainEvent/clientUpdateSubscribe.go
+++ application/domainEvent/clientUpdateSubscribe.go
@@ -5,10 +5,15 @@ import (
 	"FSchedule/domain/client"
 	"encoding/json"
 	"github.com/farseer-go/fs/core"
+	"github.com/farseer-go/fs/trace"
 )
 
 // ClientUpdateSubscribe 客户端有更新（Redis订阅）
 func ClientUpdateSubscribe(message any, _ core.EventArgs) {
+	if traceContext := trace.CurTraceContext.Get(); traceContext != nil {
+		traceContext.Ignore()
+	}
+
 	var clientDO client.DomainObject
 	err := json.Unmarshal([]byte(message.(string)), &clientDO)
 	if err != nil || clientDO.IsNil() {
--- application/domainEvent/taskGroupUpdateSubscribe.go
+++ application/domainEvent/taskGroupUpdateSubscribe.go
@@ -5,10 +5,15 @@ import (
 	"FSchedule/domain/taskGroup"
 	"encoding/json"
 	"github.com/farseer-go/fs/core"
+	"github.com/farseer-go/fs/trace"
 )
 
 // TaskGroupUpdateSubscribe 任务组有更新（Redis订阅）
 func TaskGroupUpdateSubscribe(message any, _ core.EventArgs) {
+	if traceContext := trace.CurTraceContext.Get(); traceContext != nil {
+		traceContext.Ignore()
+	}
+
 	var taskGroupDO taskGroup.DomainObject
 	err := json.Unmarshal([]byte(message.(string)), &taskGroupDO)
 	if err != nil {

56bb9ba479f818c0df0f86a3a00af33c314a9fb8
steden <1470804@qq.com>
2024-03-25 14:30:00 +0800

    添加时间判断

---
+++ application/basicapi/response/taskPlanResponse.go
@@ -0,0 +1,25 @@
+package response
+
+import (
+	"FSchedule/domain/enum"
+	"FSchedule/domain/taskGroup"
+	"github.com/farseer-go/collections"
+	"github.com/farseer-go/fs/dateTime"
+)
+
+type TaskPlanResponse struct {
+	Id          int64                                  // 主键
+	TraceId     string                                 // 上下文ID
+	Name        string                                 // 实现Job的特性名称（客户端识别哪个实现类）
+	Ver         int                                    // 版本
+	Caption     string                                 // 任务组标题
+	StartAt     string                                 // 开始时间
+	RunAt       dateTime.DateTime                      // 实际执行时间
+	RunSpeed    int64                                  // 运行耗时
+	Client      taskGroup.ClientVO                     // 客户端
+	Progress    int                                    // 进度0-100
+	Status      enum.TaskStatus                        // 状态
+	SchedulerAt dateTime.DateTime                      // 调度时间
+	Data        collections.Dictionary[string, string] // 本次执行任务时的Data数据
+	CreateAt    dateTime.DateTime                      // 任务创建时间
+}

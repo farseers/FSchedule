c8bbb236d268898c564d62e56c7b13aa1a553b90
steden <1470804@qq.com>
2024-03-26 13:48:50 +0800

    日志上报，减少redis读取

--- application/api/logReport.go
+++ application/api/logReport.go
@@ -4,6 +4,7 @@ package api
 import (
 	"FSchedule/domain/taskGroup"
 	"FSchedule/domain/taskLog"
+	"github.com/farseer-go/collections"
 	"github.com/farseer-go/fs/core/eumLogLevel"
 )
 
@@ -12,9 +13,11 @@ type logReportDTO struct {
 }
 
 type LogContent struct {
-	TaskId   int64  // 主键
-	Ver      int    // 版本
-	Name     string // 实现Job的特性名称（客户端识别哪个实现类）
+	TaskId   int64                                  // 主键
+	Ver      int                                    // 版本
+	Name     string                                 // 实现Job的特性名称（客户端识别哪个实现类）
+	Caption  string                                 // 任务标题
+	Data     collections.Dictionary[string, string] // 本次执行任务时的Data数据
 	LogLevel eumLogLevel.Enum
 	CreateAt int64
 	Content  string
@@ -24,8 +27,7 @@ type LogContent struct {
 // @post /logReport
 func LogReport(dto logReportDTO, taskGroupRepository taskGroup.Repository, taskLogRepository taskLog.Repository) {
 	for _, log := range dto.Logs {
-		taskDO := taskGroupRepository.GetTask(log.Name, log.TaskId)
-		taskLogDO := taskLog.NewDO(log.Name, taskDO.Caption, log.Ver, log.TaskId, taskDO.Data, log.LogLevel, log.Content, log.CreateAt)
+		taskLogDO := taskLog.NewDO(log.Name, log.Caption, log.Ver, log.TaskId, log.Data, log.LogLevel, log.Content, log.CreateAt)
 		taskLogRepository.Add(taskLogDO)
 	}
 }

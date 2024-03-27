0231f5dd57fd9985841a1b6c3418635ed2a0cfa7
steden <1470804@qq.com>
2024-03-27 15:12:32 +0800

    修复类型错误

--- application/domainEvent/taskFinishEvent.go
+++ application/domainEvent/taskFinishEvent.go
@@ -4,6 +4,7 @@ import (
 	"FSchedule/domain/taskGroup"
 	"github.com/farseer-go/fs/container"
 	"github.com/farseer-go/fs/core"
+	"github.com/farseer-go/fs/flog"
 )
 
 // TaskFinishEvent 任务完成事件
@@ -20,7 +21,7 @@ func TaskFinishEvent(message any, _ core.EventArgs) {
 	if do.CalculateNextAtByCron() {
 		// 任务初始化
 		do.CreateTask()
-		//flog.Debugf("任务组：%s %d 任务完成，下次执行时间：%s", do.Name, do.Task.Id, do.Task.StartAt.ToString("yyyy-MM-dd HH:mm:ss"))
+		flog.Debugf("任务组：%s %d 任务完成，下次执行时间：%s", do.Name, do.Task.Id, do.Task.StartAt.ToString("yyyy-MM-dd HH:mm:ss"))
 	}
 	taskGroupRepository.SaveAndTask(*do)
 }

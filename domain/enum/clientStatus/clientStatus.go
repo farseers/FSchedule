cec30a558641778e90d0c70045b9abe8920b5bc9
steden <1470804@qq.com>
2024-03-29 13:26:59 +0800

    调整客户端移除逻辑

--- domain/enum/clientStatus/clientStatus.go
+++ domain/enum/clientStatus/clientStatus.go
@@ -9,3 +9,19 @@ const (
 	StopSchedule             // 拒绝调度（客户端在忙）
 	Offline                  // 离线
 )
+
+func (e Enum) String() string {
+	switch e {
+	case Online:
+		return "刚上线"
+	case Scheduler:
+		return "接受调度"
+	case UnSchedule:
+		return "无法调度"
+	case StopSchedule:
+		return "拒绝调度"
+	case Offline:
+		return "离线"
+	}
+	return "刚上线"
+}

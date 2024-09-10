99baad708648314e7d4eafa2f166408a543e8c5e
steden <1470804@qq.com>
2024-09-10 20:39:52 +0800

    修改，任务组有更新订阅逻辑

--- interfaces/domainEvent/taskGroupUpdateSubscribe.go
+++ interfaces/domainEvent/taskGroupUpdateSubscribe.go
@@ -22,12 +22,20 @@ func TaskGroupUpdateSubscribe(message any, _ core.EventArgs) {
 
 	// 通知处理该任务组的服务端，需要调用客户端发起Kill请求
 	if taskGroupMonitor := domain.GetTaskGroupMonitor(taskGroupDO.Name); taskGroupMonitor != nil {
+		// 之前是运行状态，改为停止状态，则需要退出调度线程
+		if taskGroupMonitor.IsEnable && !taskGroupDO.IsEnable {
+			// 主动通知客户端，停止任务
+			taskGroupMonitor.TaskKill()
+		}
+
 		taskGroupMonitor.DomainObject.Data = taskGroupDO.Data
 		taskGroupMonitor.DomainObject.Caption = taskGroupDO.Caption
 		taskGroupMonitor.DomainObject.StartAt = taskGroupDO.StartAt
 		taskGroupMonitor.DomainObject.NextAt = taskGroupDO.NextAt
 		taskGroupMonitor.DomainObject.Cron = taskGroupDO.Cron
 		taskGroupMonitor.DomainObject.IsEnable = taskGroupDO.IsEnable
+
+		// 通知协议，有更新
 		taskGroupMonitor.Notify()
 	}
 }

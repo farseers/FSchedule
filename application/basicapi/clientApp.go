a2b06623b7a1409c26ca4da33b10347c3ae6326f
steden <@>
2024-11-20 23:42:43 +0800

    使用原生转换

--- application/basicapi/clientApp.go
+++ application/basicapi/clientApp.go
@@ -3,6 +3,8 @@ package basicapi
 
 import (
 	"FSchedule/domain/client"
+	"strconv"
+
 	"github.com/farseer-go/collections"
 	"github.com/farseer-go/fs/parse"
 )
@@ -11,7 +13,7 @@ import (
 // @get list
 func ClientList(clientRepository client.Repository) collections.List[client.DomainObject] {
 	lst := clientRepository.ToList().OrderBy(func(item client.DomainObject) any {
-		return item.Job.Name + parse.ToString(parse.ToInt(item.IsMaster)) + item.Id
+		return item.Job.Name + strconv.Itoa(parse.ToInt(item.IsMaster)) + item.Id
 	}).ToList()
 	return lst
 }

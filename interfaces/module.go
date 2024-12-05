ce6d1f06f692b914f3ddb69f34270e1fa6d27ca1
steden <@>
2024-12-05 20:20:53 +0800

    取消定时日志打印

--- interfaces/module.go
+++ interfaces/module.go
@@ -4,11 +4,12 @@ import (
 	"FSchedule/application"
 	"FSchedule/interfaces/job"
 	"context"
+	"time"
+
 	"github.com/farseer-go/fs"
 	"github.com/farseer-go/fs/modules"
 	"github.com/farseer-go/tasks"
 	"github.com/farseer-go/webapi"
-	"time"
 )
 
 type Module struct {
@@ -19,10 +20,10 @@ func (module Module) DependsModule() []modules.FarseerModule {
 }
 
 func (module Module) PostInitialize() {
-	// 打印客户端、任务组信息
-	fs.AddInitCallback("启动 【打印客户端、任务组信息】任务", func() {
-		tasks.Run("PrintInfoJob", 10*time.Second, job.PrintInfoJob, context.Background())
-	})
+	// // 打印客户端、任务组信息
+	// fs.AddInitCallback("启动 【打印客户端、任务组信息】任务", func() {
+	// 	tasks.Run("PrintInfoJob", 10*time.Second, job.PrintInfoJob, context.Background())
+	// })
 
 	// 10秒更新一次服务端信息
 	fs.AddInitCallback("启动 【每10秒更新节点活跃时间】任务", func() {
